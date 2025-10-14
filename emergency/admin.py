5e5244a8a35cb0417c2d08724e08719681f9cf7e
afiajahin21 <23101033@uap-bd.edu>
2025-10-15 00:29:05 +0600

    emergency admin

--- emergency/admin.py
+++ emergency/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import EmergencyContact, EmergencyAlert
 
-# Register your models here.
+@admin.register(EmergencyContact)
+class EmergencyContactAdmin(admin.ModelAdmin):
+	list_display = ("user", "phone_number", "created_at")
+	search_fields = ("user__username", "phone_number")
+
+@admin.register(EmergencyAlert)
+class EmergencyAlertAdmin(admin.ModelAdmin):
+	list_display = ("user", "contact", "status", "triggered_at")
+	search_fields = ("user__username", "contact__phone_number", "status")
